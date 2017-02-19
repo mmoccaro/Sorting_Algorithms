8be5a5a34e083720c937a0d1b5a4935aeef9603e
M. Moccaro <mfm2772@comcast.net>
2017-02-18 18:17:22 -0600

    Create Run_Test_Driver.bash

---
+++ Run_Test_Driver.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#  File: Run_Test_Driver.bash
+#  Type: bash script file
+#  Date: February 18, 2017
+#
+#  Description:
+#  This scripts runs the C++ Test Driver program.
+
+#Run The Test Driver
+./bin/Test_Driver.exe
