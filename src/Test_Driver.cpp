f80d7eeb680c4e45500eef2acb897b56180001e8
M. Moccaro <mfm2772@comcast.net>
2017-02-18 18:20:34 -0600

    Update Test_Driver.cpp

--- src/Test_Driver.cpp
+++ src/Test_Driver.cpp
@@ -19,6 +19,21 @@ int main()
 {
 	cout << "Sorting Algorithms" << endl;
 
+	//Create array
+	int array_to_sort[10];
+
+	//Initialize with random values
+	array_to_sort[0] = 5;
+	array_to_sort[1] = 10;
+	array_to_sort[2] = 2;
+	array_to_sort[3] = 34;
+	array_to_sort[4] = 76;
+	array_to_sort[5] = 80;
+	array_to_sort[6] = 3;
+	array_to_sort[7] = 1;
+	array_to_sort[8] = 51;
+	array_to_sort[9] = 23;	
+
 	//Exit Code
 	return 0;
 }
